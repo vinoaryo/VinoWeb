fc674462db6abb8cd56d82f05b7d2b59b334cc0a
VinoAryo <vinoaryo2000@gmail.com>
2022-05-28 19:57:46 +0700

    Adding some spice

---
+++ readme.md
@@ -0,0 +1,4 @@
+# VinoWeb
+[![Netlify Status](https://api.netlify.com/api/v1/badges/a91ab3c3-9778-49e7-97d3-d57ae90e4a16/deploy-status)](https://app.netlify.com/sites/vinoweb/deploys)
+
+VinoWeb is my personal web.
---
+++ script.js
@@ -0,0 +1,8 @@
+function darkMode() {
+  var element = document.body;
+  var footer = document.getElementById("footerDiv");
+  footer.classList.toggle("lightColor");
+  element.classList.toggle("dark-mode");
+}
+
+const JQSpan = document.getElementsByClassName('ui-btn-text').value = '';
